9a3da8eaf913a82919d8f4f4b671b542ca171129
aziz <weslatia20@gmail.com>
2025-03-17 07:00:23 +0100

    save

--- finance-app/backend/server.js
+++ finance-app/backend/server.js
@@ -1,6 +1,7 @@
 const http=require('http');
 const app =require('./app.js')
 require('dotenv').config({path:"../.env"})
+console.log("env",process.env.DB_USER)
 
 const server =http.createServer(app);
 
