081cb8dcc4ca011e1164ecfb0d660dc96f4c086f
aziz <weslatia20@gmail.com>
2025-03-17 06:32:26 +0100

    save changes

--- finance-app/backend/server.js
+++ finance-app/backend/server.js
@@ -1,11 +1,11 @@
 const http=require('http');
 const app =require('./app.js')
 require('dotenv').config({path:"../.env"})
-
+console.log("env",process.env.DB_USER)
 
 const server =http.createServer(app);
 
-const port=process.env.PORT;
+const port=process.env.PORT||3000;
 server.listen(port,'0.0.0.0',()=>{
     console.log("server running on port ",port)
 })
